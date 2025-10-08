1b868285bdb80b5c6c001c4ec4b4fe1012d3962c
fatemeh pourmand <fatemeepourmand@gmail.com>
2025-10-08 16:59:17 +0330

    Update camera38.py

--- camera38.py
+++ camera38.py
@@ -3,7 +3,7 @@
 #when said camera use 38 =mean pin38
 #lights & doors connect to pin 17,27 ,camera??? i dont have anything
 #i need a condition
-
+#i realized devices has ID or special pin with special condition and id number
 class Device:
     
     def __init__(self,location,group,device_type,device_name,pin):
