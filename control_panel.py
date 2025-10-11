171f1116def3b4593697d5b825b786d09997cbaf
fatemeh pourmand <fatemeepourmand@gmail.com>
2025-10-11 20:52:43 +0330

    Update control_panel.py

--- control_panel.py
+++ control_panel.py
@@ -3,7 +3,7 @@
 
 
 Notes:
-#at the first go to this function and complete it  def get_status_in_group it 
+#at the first go to this function and complete it  def get_status_in_group ...loading....it 
 #pay attention == all_devicess, group_name,print(f"
 
 
